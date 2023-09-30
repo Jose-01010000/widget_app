a2ee72b97ff4d2471ab78fa1311e840619ac6285
Jose-01010000 <josepuello112000@gmail.com>
2023-09-30 00:31:31 -0500

    Home screen creado✅.

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeSreen extends StatelessWidget {
+  const HomeSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter + material3'),
+      ),
+    );
+  }
+}

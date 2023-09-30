4fe4d345fd65e29822b7c9846fd7e34827124ee8
Jose-01010000 <josepuello112000@gmail.com>
2023-09-30 16:15:10 -0500

    Screens creadas y barrel actualizado.

---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SnackBarScreen extends StatelessWidget {
+  const SnackBarScreen({super.key});
+
+  static const String name = 'snack_bar_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

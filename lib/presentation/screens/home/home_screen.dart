3b72723f20d4866d6cbe259a51c884ed78cf5152
Jose-01010000 <josepuello112000@gmail.com>
2023-09-30 12:52:00 -0500

    Configuración rutas con nonbre.

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widget_app/config/menu_items/menu_items.dart';
+import 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widget_app/presentation/screens/cards/card_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  static const String name = 'home_screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,8 +50,15 @@ class _CustomListTile extends StatelessWidget {
         leading: Icon(item.icon, color: colors.primary),
         trailing: Icon(Icons.chevron_right, color: colors.primary),
         onTap: () {
+          // Rutas con flutter 
+          // Navigator.of(context).push(MaterialPageRoute(
+          //   builder: (context) => const ButtonsScreen(),
+          // ));
           // Navigator.pushNamed(context, item.link);
-          context.go(item.link);
+
+          // Rutas con go_router  
+          // context.pushNamed(CardsScreen.name);
+          context.push(item.link);
         });
   }
 }
